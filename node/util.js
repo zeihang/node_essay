310c0de84b2ba3c175002a411ebaf048cf789558
张泽涵 <zeihang@sina.com>
2019-05-29 20:15:48 +0800

    no message

--- node/util.js
+++ node/util.js
@@ -1,2 +1,11 @@
 // until.inherits 继承
 // util.promisify promise化
+
+let fs = require('mz/fs')
+let path = require('path')
+
+let read = async function (url) {
+    return await fs.readFile(url, 'utf8')
+}
+
+console.log(read())

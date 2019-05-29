e734709321acfa1b9cd40983b1b4916195c5860b
张泽涵 <zeihang@sina.com>
2019-05-29 19:30:19 +0800

    no message

---
+++ node/npm.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+/**
+"bin": {
+    "zzh": "./index.js"
+}
+ */
+
+// npm link
+
+// npm addUser
+
+// 发布
+// npm publish
+
+// npm unpublish --force
---
+++ node/util.js
@@ -0,0 +1,2 @@
+// until.inherits 继承
+// util.promisify promise化
